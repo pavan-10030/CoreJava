dee1992ef7a809e701170577f0624ffc4dd4e692
Bandike Pavan Kumar <srinivasbandik@gmail.com>
2026-03-12 22:19:34 +0530

    Document polymorphism concepts in Main.java
    Added comments explaining polymorphism and loose coupling in Java.

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -1,3 +1,21 @@
+/**
+ * ---------------Polymorphism---------------------
+ * 
+ * Polymorphism: One having many forms
+ * In java polymorphism can be acheived through:
+ *  1.Method Overloading
+ *  2.Method Overriding
+ *  3.Loose coupling
+ * 
+ * Loose Coupling:Creating parent type references to child objects is called loose coupling
+ * Using parent type reference we can invoke only inherited and overridden methods.We cannot access specialized methods.
+ */
+ 
+ 
+ /**
+  * -------------- Polymorphism through loose coupling -----------------
+  */
+
 
 class Plane
 {
