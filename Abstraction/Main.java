334a18202358ce8936203f6d4e2b7f791ca8c3f3
Bandike Pavan Kumar <srinivasbandik@gmail.com>
2026-03-12 22:10:22 +0530

    Document abstraction concept in Main.java
    Added comments explaining abstraction in Java and its implementation.

--- Abstraction/Main.java
+++ Abstraction/Main.java
@@ -1,3 +1,20 @@
+/**
+ * Abstraction in java is a mechanism for hiding implementation details and showing only neccessary info to the user.
+ * Abstraction in java can be acheived through:
+ *  1.Abstract class
+ *  2.Interfaces
+ * 
+ * Abstract classes contains abstract methods and concrete methods 
+ * Abstract methods have only method signature and no body
+ * 
+ * If a class has abstract method we should mark the whole class as abstract
+ */
+ 
+ 
+ /**
+  * -------Abstraction Implementation through abstract class---------
+  */
+
 abstract class Demo{
     abstract void greet();
 }
